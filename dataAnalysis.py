096723c80ea6f3fb6744d1b95e2a22ea9b177cff
Erick <ericksilvarodriguez@gmail.com>
2017-03-17 13:24:04 -0400

    histogramas

---
+++ dataAnalysis.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import csv
+from textblob import TextBlob
+import pandas
+import cPickle
+import numpy as np
+
+# (faltan imports)
+
+messages = pandas.read_csv('DATA/train_2000.label', sep="\t",quoting=csv.QUOTE_NONE,
+							names=["label","sublabel","question"])
+
+messages['length'] = messages['question'].map(lambda text: len(text))
+
+messages.hist(column='length',by='label',bins = 40)
+plt.show()
