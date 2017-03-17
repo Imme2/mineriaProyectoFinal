5db98c9a02ce41b53e57951d654842791b9a310f
Erick <ericksilvarodriguez@gmail.com>
2017-03-17 15:50:23 -0400

    codigo de tfidf

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -4,12 +4,33 @@ from textblob import TextBlob
 import pandas
 import numpy as np
 
-# (faltan imports)
-messages = pandas.read_csv('DATA_TAB/train_3000.label', sep="\t",quoting=csv.QUOTE_NONE,encoding = "ISO-8859-1",
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
-							names=["label","sublabel","question"])
 
-messages['length'] = messages['question'].map(lambda text: len(text))
+def split_into_tokens(message):
+    return TextBlob(message).words
 
-messages.hist(column='length',by='label',bins = 40)
-plt.show()
+def split_into_lemmas(message):
+    message = message.lower()
+    words = TextBlob(message).words
+    # for each word, take its "base form" = lemma 
+    return [word.lemma for word in words]
+
+if __name__ == '__main__':
+	# (faltan imports)
+	messages = pandas.read_csv('DATA_TAB/train_3000.tab', sep="\t",quoting=csv.QUOTE_NONE,
+								encoding = "ISO-8859-1",
+								names=["label","sublabel","question"])
+
+	messages['length'] = messages['question'].map(lambda text: len(text))
+
+	messages.hist(column='length',by='label',bins = 40)
+	plt.show()
+
+	bow_transformer = CountVectorizer(analyzer=split_into_lemmas).fit(messages['question'])
+
+	messages_bow = bow_transformer.transform(messages['question'])
+
+	tfidf_transformer = TfidfTransformer().fit(messages_bow)
+	messages_tfidf = tfidf_transformer.transform(messages_bow)
+	# Aqui en messages_tfidf y con el transformer ya tenemos todas las palabras
