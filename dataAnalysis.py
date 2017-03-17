5d3d4b9e1fd8a18353567fdec974376748944a9b
Gabriel Gimenez <12-11006@usb.ve>
2017-03-17 15:11:01 -0400

    Fix tab

--- dataAnalysis.py
+++ dataAnalysis.py
@@ -2,12 +2,11 @@ import matplotlib.pyplot as plt
 import csv
 from textblob import TextBlob
 import pandas
-import cPickle
 import numpy as np
 
 # (faltan imports)
 
-messages = pandas.read_csv('DATA/train_2000.label', sep="\t",quoting=csv.QUOTE_NONE,
+messages = pandas.read_csv('DATA_TAB/train_2000.label', sep="\t",quoting=csv.QUOTE_NONE, encoding = "ISO-8859-1",
 							names=["label","sublabel","question"])
 
 messages['length'] = messages['question'].map(lambda text: len(text))
